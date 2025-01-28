46ed04a644e0bba3b9195a64a06df4a298ed7479
立辰科技 <leonis.tek@gmail.com>
2025-01-28 16:59:30 +0800

    new

---
+++ example/LSR01_GetCmdNumber/LSR01_GetCmdNumber.ino
@@ -0,0 +1,50 @@
+//
+//	本範例是使用Arduino透過LSR-01 AI語音辨識模組進行語音命令辨識,
+//	並進行D13亮滅控制,當使用者下[命令1]時,D13 LED燈亮起,下[命令2]時,D13 LED燈熄滅。
+//
+//	接線方式: 
+//	LSR-01 模組		Arduino模組
+//		5V 		->		5V
+//		RX1		->		D12
+//		TX1		->		D11
+//		GND		->		GND
+//
+//	使用的函式庫: https://github.com/LeonisTek/LSR-01
+//
+//	立辰科技 28.JAN.2025
+//
+
+
+#include "LSR01.h"
+
+//定義LSR01接腳RX,TX
+LSR01 LSR(11,12); 
+
+void setup() {
+  
+  //初始化 LSR01模組
+  LSR.begin();
+  
+  pinMode(13,OUTPUT);
+
+}
+//宣告用來存放回傳聲控命令的
+uint8_t LSR01_CMD=0;
+
+void loop() {
+
+  
+  //判斷cmd,當cmd =1時 代表有讀到新的聲控命令(0為沒有讀到新命令),並把命令號碼帶入LSR01_CMD中
+  if(LSR.GetCMD(&LSR01_CMD);){
+	//顯示讀到的命令編號
+    Serial.println(LSR01_CMD);
+  }
+  
+  //判斷命令編號,並拿來控制D13 LED
+  if(LSR01_CMD == 1){
+    digitalWrite(13,1);
+  }else if(LSR01_CMD == 2){
+    digitalWrite(13,0);
+  }
+
+}
